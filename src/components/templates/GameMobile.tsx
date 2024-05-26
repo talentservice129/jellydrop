ac8fa172cf4d397901e4b96dcada4d5aee973329
talentservice129 <talentservice129@gmail.com>
2024-05-26 20:53:25 +0300

    change pos

--- src/components/templates/GameMobile.tsx
+++ src/components/templates/GameMobile.tsx
@@ -93,7 +93,7 @@ export const GameMobile: VFC = () => {
                 </div>
             )} */}
             {/* {Swipe !== 'on' && ( */}
-            <div className="flex absolute bottom-12 left-4 right-4">
+            <div className="flex px-4 mt-4">
                 <GameControls className="w-full" transparent={true} />
             </div>
             {/* )} */}
